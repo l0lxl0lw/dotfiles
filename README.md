d111a8c0c49a6bbf1d74ba6a4f8359794b5fc295
l0lxl0lw <l0lxl0lw@gmail.com>
2025-12-18 14:58:24 -0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Dotfiles
+
+## Setups
+- run `./deploy.sh` initially
---
+++ zsh/awscli.zsh
@@ -0,0 +1,25 @@
+export PATH=/usr/local/bin/:$PATH
+autoload bashcompinit && bashcompinit
+autoload -Uz compinit && compinit
+complete -C $(which aws_completer) aws
+
+
+logs() {
+    local operation=$1
+    if [[ $operation == "list" ]]; then
+        aws logs describe-log-groups | jq '.logGroups[] | select(.logGroupName | contains("CODETEST") | not)' | jq '.logGroupName'
+    elif [[ $operation == "query" ]]; then
+        local queryId=$(aws logs start-query --log-group-name $2 --start-time $(date -v-1d +%s) --end-time $(date +%s) --query-string $3 | jq -r .queryId)
+        echo ">> Query ID: $queryId"
+        local resultStatus="Running"
+        while [[ $resultStatus == "Running" ]]; do
+            echo ">> Result status: $resultStatus"
+            local result=$(aws logs get-query-results --query-id $queryId)
+            local resultStatus=$(echo $result | jq -r .status)
+        done
+        echo ">> Result status: $resultStatus"
+        echo $result | jq '.results[] | .[] | select(.field=="@message")' | jq -r .value
+    fi
+}
+
+fpath=(~/.zsh/completion $fpath)
